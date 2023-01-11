7669e2b32de996d3b816d51e7ebf1f37a9f8bfd8
mateuszkasprzyk <mateuszkasprzyk2000@gmail.com>
2023-01-11 21:26:38 +0100

    VS solution fix?

--- GaussianBlur/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ GaussianBlur/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -110,7 +110,7 @@ namespace GaussianBlur {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/GaussianBlur;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/GaussianBlur;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
